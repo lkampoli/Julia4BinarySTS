794cf30cc8ac044065e3d65022ee2c40b7483621
Documenter.jl <documenter@juliadocs.github.io>
2022-04-12 04:48:45 +0000

    build based on b8dfc0d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Julia4BinarySTS","category":"page"},{"location":"#Julia4BinarySTS","page":"Home","title":"Julia4BinarySTS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Julia4BinarySTS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Julia4BinarySTS]","category":"page"}]
+}
